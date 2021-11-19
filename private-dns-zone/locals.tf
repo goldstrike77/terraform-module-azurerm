ae07167164dd67480f43194f218e61300c40483c
suzhetao <suzhetao@gmail.com>
2021-11-19 18:07:44 +0800

    modified:   private-dns-zone/locals.tf 	modified:   private-dns-zone/resources.tf

--- private-dns-zone/locals.tf
+++ private-dns-zone/locals.tf
@@ -8,14 +8,24 @@ locals {
       }
     ] if length(s.link[*]) > 0
   ])
+  dns_flat_vnet = flatten([
+    for s in var.res_spec.private_dns_zone[*] : [
+      for t in s.link[*] : [
+        for u in s.name[*] : {
+          name = u
+          tags = lookup(s, "tags", null)
+          registration = lookup(s, "registration", false)
+          resource_group = t.resource_group
+          virtual_network = t.virtual_network
+        }
+      ]
+    ] if length(s.link[*]) > 0
+  ])
   dns_flat = flatten([
     for s in var.res_spec.private_dns_zone[*] : [
-      for t in s.link[*] : {
-        name = s.name
+      for t in s.name[*] : {
+        name = t
         tags = lookup(s, "tags", null)
-        registration = lookup(s, "registration", false)
-        resource_group = t.resource_group
-        virtual_network = t.virtual_network
       }
     ] if length(s.link[*]) > 0
   ])
--- private-dns-zone/resources.tf
+++ private-dns-zone/resources.tf
@@ -1,23 +1,23 @@
 # Access information about an existing Virtual Network.
 data "azurerm_virtual_network" "virtual_network" {
   for_each = { for s in distinct(local.vnet_flat) : format("%s", s.virtual_network) => s }
-  name = each.value.virtual_network
+  name = each.key
   resource_group_name = each.value.resource_group
 }
 
 # Manage Private DNS zones within Azure DNS.
 resource "azurerm_private_dns_zone" "private_dns_zone" {
-  for_each = { for s in var.res_spec.private_dns_zone[*] : format("%s", s.name) => s }
-  name = each.value.name
+  for_each = { for s in distinct(local.dns_flat) : format("%s", s.name) => s }
+  name = each.key
   resource_group_name = var.res_spec.rg[0].name
   tags = merge(var.tags,each.value.tags)
 }
 
 # Manage Private DNS zone Virtual Network Links.
 resource "azurerm_private_dns_zone_virtual_network_link" "private_dns_zone_virtual_network_link" {
-  for_each = { for s in local.dns_flat : format("%s%s%s", s.name, s.resource_group, s.virtual_network) => s }
+  for_each = { for s in local.dns_flat_vnet : format("%s%s%s", s.name, s.resource_group, s.virtual_network) => s }
   name = "link-to-${each.value.virtual_network}"
-  resource_group_name  = var.res_spec.rg[0].name
+  resource_group_name = var.res_spec.rg[0].name
   private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone[each.value.name].name
   registration_enabled = each.value.registration
   tags = merge(var.tags,each.value.tags)
